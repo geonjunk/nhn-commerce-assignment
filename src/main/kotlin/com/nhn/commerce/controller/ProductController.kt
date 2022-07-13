aeb641238b0fe0adfc4abb86779a11215b3f0306
geonjunk <koeunjun7@gmail.com>
2022-07-13 11:36:16 +0900

    MVC 과제

--- src/main/kotlin/com/nhn/commerce/controller/ProductController.kt
+++ src/main/kotlin/com/nhn/commerce/controller/ProductController.kt
@@ -1,6 +1,8 @@
 package com.nhn.commerce.controller
 
+import com.nhn.commerce.model.Product
 import com.nhn.commerce.service.ProductService
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
@@ -12,15 +14,39 @@ class ProductController(
     @GetMapping("/product")
     fun getProductList(model: Model): String {
         model.addAttribute("productList", productService.findProductList())
+        model.addAttribute("product",Product(0,"", null,0,null))
         return "product"
     }
 
     // TODO (상품 상세 조회 기능 + Exception 처리)
+    @GetMapping("/product/{productNo}")
+    fun getProductDetail(@PathVariable (name="productNo") productNo:Int,model:Model):String{
+        model.addAttribute("productDetail",productService.findProductDetail(productNo))
+        return "detailProduct"
+    }
 
-    // TODO (상품 추가 기능)
 
-    // TODO (상품 수정 기능 + Exception 처리)
+    // TODO (상품 추가 기능) 완료
+    @PostMapping("/product")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun addProduct(
+        product:Product,model: Model):String{
+        productService.addProduct(product)
+        return getProductList(model)
+    }
+
+
+    // TODO (상품 수정 기능 + Exception 처리) 완료
+    @PostMapping("/product/update")
+    fun updateProduct(product: Product,model: Model):String{
+        productService.updateProduct(product.productNo,product.productName,product.salePrice)
+        return "redirect:/product"
+    }
 
     // TODO (상품 삭제 기능 + Exception 처리)
-
+    @PostMapping("/product/delete")
+    fun deleteProduct(product: Product,model: Model):String{
+            productService.deleteProduct(product.productNo)
+        return  "redirect:/product"
+    }
 }
--- src/main/kotlin/com/nhn/commerce/service/ProductService.kt
+++ src/main/kotlin/com/nhn/commerce/service/ProductService.kt
@@ -3,10 +3,16 @@ package com.nhn.commerce.service
 import com.nhn.commerce.model.Product
 import com.nhn.commerce.repository.ProductRepository
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class ProductService(
     private val productRepository: ProductRepository,
 ) {
     fun findProductList(): List<Product> = productRepository.findProductList()
+    fun findProductDetail(productNo:Int): Product = productRepository.findProductDetail(productNo)
+    fun addProduct(product: Product):List<Product> = productRepository.addProduct(product.productNo,product.productName,product.salePrice,LocalDateTime.now())
+    fun updateProduct(productNo: Int,productName:String,salePrice:Int):List<Product> = productRepository.updateProduct(productNo,productName,salePrice,LocalDateTime.now())
+
+    fun deleteProduct(productNo: Int):List<Product> = productRepository.deleteProduct(productNo)
 }
